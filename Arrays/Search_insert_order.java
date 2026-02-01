acf3f656c66328509a2d5cb6f3fda6ce3a08db49
Mukesh Kannan <mukeshkannan3112005@gmail.com>
2026-02-01 13:48:45 +0530

    solved Search insertion order using array

---
+++ Arrays/Search_insert_order.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int searchInsert(int[] arr, int key) {
+        int low = 0;     //left element
+        int high = arr.length - 1; //right element
+
+        while (low <= high) {   
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                return mid;   // key found
+            } 
+            else if (arr[mid] < key) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return high+1;   //if key not found return the were the element is inserted in order
+        
+    }
+}
+
+//recursive solution
+class Solution {
+    public int searchInsert(int[] arr, int key) {
+       return binarySearch(arr,0,arr.length-1,key);
+        
+    }
+
+    public static int binarySearch(int[] arr, int low, int high, int key) {
+
+        if (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] < key)
+                return binarySearch(arr, mid + 1, high, key);
+            else
+                return binarySearch(arr, low, mid - 1, key);
+        }
+
+        return high+1;
+    }
+}
