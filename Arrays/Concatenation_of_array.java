a2127e8477ce03cf71ea37a46e438117d4537eb9
Mukesh Kannan <mukeshkannan3112005@gmail.com>
2026-02-02 05:40:53 +0530

    Solved concatenation of same array problem

---
+++ Arrays/Concatenation_of_array.java
@@ -0,0 +1,11 @@
+//concatenation of same array
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int [] ans=new int[nums.length*2];
+        for(int i=0;i<nums.length*2;i++)
+        {
+            ans[i]=nums[i%nums.length]; // here modulo operator is used to repeat the array to concatenate
+        }
+        return ans;
+    }
+}
