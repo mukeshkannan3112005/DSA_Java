aa21a6711b838ca1ee8232c21020d7c6c854e222
Mukesh Kannan <mukeshkannan3112005@gmail.com>
2026-02-21 05:56:28 +0530

    solved rotate linked list problem

---
+++ LInked_List/Rotate_linkedlist.java
@@ -0,0 +1,47 @@
+//https://leetcode.com/problems/rotate-list/description/
+//time complexity: O(n)
+//space complexity: O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        // Step 1: Find length and tail
+        ListNode tail = head;
+        int length = 1;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        // Step 2: Make circular
+        tail.next = head;
+
+        // Step 3: Find new tail
+        k = k % length;
+        int stepsToNewTail = length - k - 1;
+
+        ListNode newTail = head;
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        // Step 4: Break the circle
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
