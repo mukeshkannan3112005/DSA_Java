249e1f9c731a1948c716373b8e8072f0ee1eefba
Mukesh Kannan <mukeshkannan3112005@gmail.com>
2026-03-05 22:08:29 +0530

    solved bulid an array with stack operations problem

---
+++ Stack/Build_an_Array_With_Stack_Operations.java
@@ -0,0 +1,29 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Build_an_Array_With_Stack_Operations {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> operations = new ArrayList<>();
+        
+        int index = 0; // pointer for target array
+        
+        for (int num = 1; num <= n && index < target.length; num++) {
+            
+            // Always push the number
+            operations.add("Push");
+            
+            // If this number matches target, keep it
+            if (num == target[index]) {
+                index++;
+            } 
+            // Otherwise, pop it
+            else {
+                operations.add("Pop");
+            }
+        }
+        
+        return operations;
+    }
+}
