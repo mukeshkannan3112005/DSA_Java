124c968c0b3fb81e506d5a284af03b7c3a6ba314
Mukesh Kannan <mukeshkannan3112005@gmail.com>
2026-03-21 22:32:02 +0530

    solved Roman to integer and integer to roman problem

--- Arrays/IntegerToRoman.java
+++ Arrays/IntegerToRoman.java
@@ -1,10 +1,13 @@
+//https://leetcode.com/problems/integer-to-roman/description
+// time complexity: O(n)
+// space complexity: O(1)
 class IntegerToRoman {
     public String intToRoman(int num) {
         final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder sb = new StringBuilder();
-
+        //We iterate through the values array, and for each value, we check if the input number is greater than or equal to that value. If it is, we append the corresponding symbol to the StringBuilder and subtract the value from the input number. We repeat this process until the input number becomes zero, at which point we have constructed the Roman numeral representation of the original integer. Finally, we return the resulting string.   
         for (int i = 0; i < values.length; ++i) {
             if (num == 0)
                 break;
--- Arrays/RomanToInteger.java
+++ Arrays/RomanToInteger.java
@@ -1,3 +1,8 @@
+//https://leetcode.com/problems/roman-to-integer/description
+// time complexity: O(n)
+// space complexity: O(1)
+import java.util.HashMap;
+import java.util.Map;
 class RomanToInteger {
     public int romanToInt(String s) {
         int res = 0;
@@ -9,7 +14,7 @@ class RomanToInteger {
         roman.put('C', 100);
         roman.put('D', 500);
         roman.put('M', 1000);
-
+        //We iterate through the input string, and for each character, we compare its corresponding value with the value of the next character. If the current character's value is less than the next character's value, it means that we need to subtract the current character's value from the result. Otherwise, we add the current character's value to the result. Finally, we add the value of the last character to the result and return it as the final integer representation of the Roman numeral. 
         for (int i = 0; i < s.length() - 1; i++) {
             if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
                 res -= roman.get(s.charAt(i));
