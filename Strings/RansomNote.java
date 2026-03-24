c0d0c930ef22d0384cea3f5780620184c8bc3718
Mukesh Kannan <mukeshkannan3112005@gmail.com>
2026-03-24 21:38:37 +0530

    solved ransomNode problem

--- Strings/RansomNote.java
+++ Strings/RansomNote.java
@@ -1,13 +1,15 @@
 package Strings;
-
+//https://leetcode.com/problems/ransom-note/description/
+// time complexity: O(n) because we are traversing the magazine and ransomNote strings once each      
+// space complexity: O(1) because we are using a fixed-size array of length 26 to count the characters  
 class RansomNote {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] count = new int[26];
-
+        // Count the frequency of each character in the magazine
         for (char c : magazine.toCharArray()) {
             count[c - 'a']++;
         }
-
+        // Check if the ransom note can be constructed from the magazine
         for (char c : ransomNote.toCharArray()) {
             if (count[c - 'a'] == 0) {
                 return false;
