8ee7d12f26689dd18b2b871973b41c7edd36bec7
Mukesh Kannan <mukeshkannan3112005@gmail.com>
2026-04-21 21:54:21 +0530

    Solved Add two interger problem

---
+++ Math/AddTwoIntegers.java
@@ -0,0 +1,11 @@
+package Math;
+//https://leetcode.com/problems/add-two-integers/description/
+// time complexity: O(1)
+// space complexity: O(1)
+// approach: we can simply return the sum of the two integers.
+// technique used: math
+class  AddTwoIntegers{
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
