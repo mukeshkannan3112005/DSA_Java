8486974e53c62e5c7c04f3f08f3aa46719049997
Mukesh Kannan <mukeshkannan3112005@gmail.com>
2026-06-07 11:25:04 +0530

    Solved Flatten Binary Tree to linked list problem

---
+++ Tree/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,32 @@
+//  https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+// space complexity: o(n)
+// time complexity: o(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class FlattenBinaryTreeToLinkedList {
+    TreeNode prev=null;
+    public void flatten(TreeNode root) {
+        if(root==null)
+        {
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right=prev;
+        root.left=null;
+        prev=root;
+    }
+}
